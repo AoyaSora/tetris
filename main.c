569f99bbf1a6f1f1abea0f4c41d33d6f110d385f
AoyaSora <s24g1125jk@s.chibakoudai.jp>
2026-06-20 16:19:31 +0900

    squreの描画がうまくいっていない．エスケープシーケンスと配列の関係を理解するのが解決策だと考えている

--- main.c
+++ main.c
@@ -5,14 +5,25 @@
 
 #define COLS 50
 #define ROWS 30
+#define BLOCKSIZE 5
 
+void printBlock(const int block[][BLOCKSIZE], int tetY, int tetX);
+int startx = (COLS -1) / 2 + 1;
+int starty = 3;
 int main() {
     // Hide cursor
     // printf("\e[?25l");
-
+        const int square[BLOCKSIZE][BLOCKSIZE] = {
+            {0,0,0,0,0},
+            {0,0,0,0,0},
+            {0,0,1,1,0},
+            {0,0,1,1,0},
+            {0,0,0,0,0},
+        };
+  
 
     int quit = 0;
-    int tetris[5][5];
+    int tetris[BLOCKSIZE][BLOCKSIZE];
     int board[ROWS][COLS] = {0};
     while (!quit)
     {
@@ -39,8 +50,6 @@ int main() {
 
         int gameover = 0;
         int xdir = 0, ydir = 1;
-        int startx = (COLS -1) / 2 + 1;
-        int starty = 3;
         int tetY = starty, tetX = startx; // it's base on EscapeSequene
         // top left "·" potition
         printf("\e[%i;%iH❤",starty,2); 
@@ -50,12 +59,15 @@ int main() {
             if(tetY!=starty) printf("\e[%i;%iH·",tetY-1, tetX);
 
             // Draw tetris
-            printf("\e[%i;%iH#",tetY, tetX);
+            printBlock(square,tetY,tetX);
+            // printf("\e[%i;%iH#",tetY, tetX);
+
+            // Touch floar or under Tetris
             if(tetY == 2 + ROWS || board[tetY-2][tetX-2] == 1) {
                 board[tetY-3][tetX-2] = 1;
                 tetY = starty; tetX = startx;
             }
-            tetY++;
+            // tetY++;
 
             fflush(stdout);
             usleep(5 * 1000000 / 60);
@@ -65,4 +77,16 @@ int main() {
     // Show cursor
     printf("\e[?25h");
     return 0;
+}
+
+void printBlock(const int block[][BLOCKSIZE], int tetY, int tetX) {
+    for(int j = 0; j < BLOCKSIZE; j++) {
+        int y = tetY - 2 + j;
+        for(int i = 0; i < BLOCKSIZE; i++) {
+            int x = tetX - 2 + i;
+            if( block[j][i] == 1 || starty< y) {
+                printf("\e[%i;%iH#",y,x);
+            }
+        }
+    }
 }
